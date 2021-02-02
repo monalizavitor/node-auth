12ee7112d10a0afb88ac0bf6f91cd98bb2924563
monaliza-vitor <vitormonaliza@gmail.com>
2021-02-02 11:13:13 -0300

    routes

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ const routes = require('express').Router()
 
 const SessionController = require('./app/controllers/SessionController')
 
-onmsgesturestart.post('/sessions', (req, res))
 routes.post('/sessions', SessionController.store)
 
 
