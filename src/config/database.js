146a5580afbffe5467a5e02961c1e733e5097fa6
monaliza-vitor <vitormonaliza@gmail.com>
2021-02-02 10:33:57 -0300

    iniciando controller

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,12 @@
 require('dotenv').config({
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
-
 })
 
 module.exports = {
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.NAME,
+    database: process.env.DB_NAME,
     dialect: process.env.DB_DIALECT || 'mysql',
     storage: './__tests_/database.sqlite',
     operatorsAliases: false,
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 const routes = require('express').Router()
 
+const SessionController = require('./app/controllers/SessionController')
 
-
-
-
+onmsgesturestart.post('/sessions', (req, res))
+routes.post('/sessions', SessionController.store)
 
 
 module.exports = routes
